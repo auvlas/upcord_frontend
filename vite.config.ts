6448452c5bd057cd81642b79724742cc6ed9a389
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2026-09-24 03:08:42 +0400

    feat(vite.config.ts): Config server and preview

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        strictPort: false,
+        host: true,
+        https: true
+    },
+    preview: {
+        port: 443,
+        strictPort: true,
+        host: true,
+        https: true
+    },
+    plugins: [
+        react(),
+        mkcert()
+    ]
 })
